87bbc27a102516ba9c41d5c4240a7501e3d4c7f0
LeducJean <jean.leduc80@gmail.com>
2024-05-03 16:04:40 +0200

    test module de test

--- CodeDMX/projet-dmx/src/App.js
+++ CodeDMX/projet-dmx/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/* import React from 'react';
 import './App.css';
 import StudiomodePage from './StudiomodePage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -9,6 +9,26 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<StudiomodePage />} />
+        <Route path="/mode-configuration" element={<ConfigurationModePage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+*/
+
+import React from 'react';
+import './App.css';
+import SQLTestPage from './SQLTestPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ConfigurationModePage from './ConfigurationmodePage';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<SQLTestPage />} />
         <Route path="/mode-configuration" element={<ConfigurationModePage />} />
       </Routes>
     </Router>
